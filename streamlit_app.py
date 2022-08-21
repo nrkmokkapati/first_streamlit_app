403ed56941a3373373c519dec6f1efaeac0737ce
nrkmokkapati <108636681+nrkmokkapati@users.noreply.github.com>
2022-08-20 21:42:04 -0400

    Update streamlit_app.py
    Added a break fast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.title('Breakfast menu')
+streamlit.title('Omega3 and Oatmeal buleberry')
+streamlit.title('Kales Spinach and Rocket Smoothie')
+streamlit.title('Hard Boiled Free Range Egg')
+
